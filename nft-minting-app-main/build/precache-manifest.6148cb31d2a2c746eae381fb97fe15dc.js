05d0c701a4a63499c4a9b032cccb265e4a474f4d
boneDryC <103467074+boneDryC@users.noreply.github.com>
2022-05-27 20:07:52 +1000

    Add files via upload

---
+++ nft-minting-app-main/build/precache-manifest.6148cb31d2a2c746eae381fb97fe15dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5547b29dffbeda85810",
+    "url": "/static/css/main.41645861.chunk.css"
+  },
+  {
+    "revision": "c5547b29dffbeda85810",
+    "url": "/static/js/main.bd8aa232.chunk.js"
+  },
+  {
+    "revision": "e91bda4cae00483fb059",
+    "url": "/static/js/runtime~main.c7014197.js"
+  },
+  {
+    "revision": "9ab0edda664d34b8b0c9",
+    "url": "/static/js/2.2c055e3a.chunk.js"
+  },
+  {
+    "revision": "6a69bdf8870abf91c28e",
+    "url": "/static/js/3.e92ae49a.chunk.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  },
+  {
+    "revision": "5bd21a7107faafca30ca593a7b3f2282",
+    "url": "/index.html"
+  }
+];
